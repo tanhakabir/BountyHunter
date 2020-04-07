7bcf81fab6703ebcb9537c933a64110330bbc084
Tanha <tanhakabir.ca@gmail.com>
2020-04-07 00:13:42 -0700

    Move app to root dir
    https://stackoverflow.com/questions/43362014/heroku-no-default-language-could-be-detected-for-this-app-error-thrown-for-no

---
+++ build/precache-manifest.dfa9a7dfea8036ab9eabf018824e79ef.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2ea2ed570f0cb396a4ca3c410e51db79",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0039d030022ac6ebc30b",
+    "url": "/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "5ee305a476baf1cda398",
+    "url": "/static/js/2.ad3a0fcf.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.ad3a0fcf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0039d030022ac6ebc30b",
+    "url": "/static/js/main.ea8771ba.chunk.js"
+  },
+  {
+    "revision": "da6ec9ab48d9f7373cef",
+    "url": "/static/js/runtime-main.91458ec2.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
