a2562a8424b692680481c1682c233ba6f34fde89
Tanha <tanhakabir.ca@gmail.com>
2020-04-07 23:06:29 -0700

    complete dummy chat

--- server/index.js
+++ server/index.js
@@ -1,10 +1,28 @@
-const http = require('http')
-const app = require('./config')
-const Server = http.Server(app)
-const PORT = process.env.PORT || 8000
-const io = require('socket.io')(Server)
+const http = require('http');
+const express = require('express');
+const path = require('path');
 
-Server.listen(PORT, () => console.log('Game server running on:', PORT))
+const app = express();
+const Server = http.Server(app);
+const io = require('socket.io')(Server);
+
+const SERVE_PORT = process.env.PORT || 443
+
+// SERVING WEB CONTENT
+app.use(express.static(path.join(__dirname, 'build')));
+
+app.get('/', function(req, res) {
+	res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
+
+// app.use(cors())
+
+app.listen(SERVE_PORT);
+
+
+// CONNECTING TO CLIENT
+const SOCKET_PORT = 9001
+Server.listen(SOCKET_PORT, () => console.log('Game server running on:', SOCKET_PORT))
 
 io.on('connection', function(socket){
 	socket.on('chat message', function(msg){
---
+++ src/App.jsx
@@ -0,0 +1,53 @@
+import React from 'react';
+import io from 'socket.io-client';
+import './App.css';
+
+var socket = io('http://localhost:9001');
+
+class App extends React.Component {
+  state = {};
+
+  constructor(props) {
+    super(props);
+    this.state = {value: '',
+                  messages: ['abc', 'bcd']};
+
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  componentDidMount() {
+    socket.on('chat message', (message) => {
+      console.log("received", message)
+      this.setState({messages: [...this.state.messages, message]})
+    });
+  }
+
+  handleChange(event) {
+    this.setState({value: event.target.value});
+  }
+
+  handleSubmit(event) {
+    socket.emit('chat message', this.state.value);
+    this.setState({value: ""})
+    event.preventDefault();
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <ul>
+          {this.state.messages.map((msg) =>
+            <li>{msg}</li>
+          )}
+        </ul>
+        <form onSubmit={this.handleSubmit}>
+          <input type="text" value={this.state.value} onChange={this.handleChange} />
+          <button>Send</button>
+        </form>
+      </div>
+    );
+  }
+}
+
+export default App;
