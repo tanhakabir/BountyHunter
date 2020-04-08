7cdc0dc201c7b2c4e5ea8808f48ad104f072ea4b
tanhakabir <tanhakabir.ca@gmail.com>
2020-04-07 23:31:47 -0700

    Fix heroku serving content 404 bug (#10)
    * Socket.io chat example (#5)
    
    * start server checkpoint
    
    * Update config.js
    
    * complete dummy chat
    
    * Update package.json script for heroku builidng server
    
    * change build path for server serving content (#9)
    
    * change build path
    
    * rename

--- server/index.js
+++ server/index.js
@@ -9,20 +9,20 @@ const io = require('socket.io')(Server);
 const SERVE_PORT = process.env.PORT || 443
 
 // SERVING WEB CONTENT
-app.use(express.static(path.join(__dirname, 'build')));
+app.use(express.static(path.join(__dirname, '../build')));
 
 app.get('/', function(req, res) {
-	res.sendFile(path.join(__dirname, 'build', 'index.html'));
+	res.sendFile(path.join(__dirname, '../build', 'index.html'));
 });
 
 // app.use(cors())
 
-app.listen(SERVE_PORT);
+app.listen(SERVE_PORT, () => console.log('Web server running on: ', SERVE_PORT));
 
 
 // CONNECTING TO CLIENT
-const SOCKET_PORT = 9001
-Server.listen(SOCKET_PORT, () => console.log('Game server running on:', SOCKET_PORT))
+const SIGNAL_PORT = 9001
+Server.listen(SIGNAL_PORT, () => console.log('Signalling server running on:', SIGNAL_PORT))
 
 io.on('connection', function(socket){
 	socket.on('chat message', function(msg){
